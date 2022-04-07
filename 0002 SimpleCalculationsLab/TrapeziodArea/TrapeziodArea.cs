f560313b6893930d189cae0192140252c0668001
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 01:06:03 +0300

    Chapter 2.0

---
+++ 0002 SimpleCalculationsLab/TrapeziodArea/TrapeziodArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class TrapeziodArea
+{
+    static void Main()
+    {
+        double b1;
+        double b2;
+        double h;
+        double area;
+
+        b1 = double.Parse(Console.ReadLine());
+        b2 = double.Parse(Console.ReadLine());
+        h = double.Parse(Console.ReadLine());
+
+        area = (b1 + b2) * h / 2;
+
+        Console.WriteLine("Trapezion area = " + area);
+    }
+}
