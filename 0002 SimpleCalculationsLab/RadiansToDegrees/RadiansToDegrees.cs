290af81643f976a6640cf6cfd3a30b734ce119a9
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 01:04:54 +0300

    Chapter 2.0

---
+++ 0002 SimpleCalculationsLab/RadiansToDegrees/RadiansToDegrees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class RadiansToDegrees
+{
+    static void Main()
+    {
+        double rad;
+        double deg;
+
+        rad = double.Parse(Console.ReadLine());
+
+        deg = rad * 180 / Math.PI;
+
+        Console.WriteLine((int)(Math.Round(deg, 2)));
+    }
+}
