19edbcfec0ad0e201eb510766e62a38edf9ea999
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 01:06:27 +0300

    Chapter 2.0

---
+++ 0002 SimpleCalculationsLab/TriangleArea/TriangleArea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class TriangleArea
+{
+    static void Main()
+    {
+        double a;
+        double h;
+        double area;
+
+        a = double.Parse(Console.ReadLine());
+        h = double.Parse(Console.ReadLine());
+
+        area = a * h / 2;
+
+        Console.WriteLine(Math.Round(area, 2));
+    }
+}
