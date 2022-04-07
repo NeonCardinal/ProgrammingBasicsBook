5bea776f4625ae1e06e069b41562be9d09ae876b
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 01:07:19 +0300

    Chapter 2.0

---
+++ 0002 SimpleCalculationsLab/USDToBGN/USDToBGN.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class USDToBGN
+{
+    static void Main()
+    {
+        double usd;
+        double bgn;
+
+        usd = double.Parse(Console.ReadLine());
+
+        bgn = usd * 1.79549;
+
+        Console.WriteLine(Math.Round(bgn, 2));
+    }
+}
