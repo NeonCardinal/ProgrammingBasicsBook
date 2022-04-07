d89e935604cb394034067327d67279ab6681b666
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 01:06:55 +0300

    Chapter 2.0

---
+++ 0002 SimpleCalculationsLab/TwoDRectangleArea/TwoDRectangleArea.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class TwoDRectangleArea
+{
+    static void Main()
+    {
+        double x1;
+        double x2;
+        double y1;
+        double y2;
+        double width;
+        double height;
+
+        x1 = double.Parse(Console.ReadLine());
+        y1 = double.Parse(Console.ReadLine());
+        x2 = double.Parse(Console.ReadLine());
+        y2 = double.Parse(Console.ReadLine());
+
+        width = Math.Max(x1, x2) - Math.Min(x1, x2);
+        height = Math.Max(y1, y2) - Math.Min(y1, y2);
+
+        Console.WriteLine(width * height);
+        Console.WriteLine((width + height) * 2);
+    }
+}
