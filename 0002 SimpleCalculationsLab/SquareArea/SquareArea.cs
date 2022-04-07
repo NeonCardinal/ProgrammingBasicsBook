61cdfa741c3cdf1da5766e7a3d75d19d14c7681b
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 01:05:18 +0300

    Chapter 2.0

---
+++ 0002 SimpleCalculationsLab/SquareArea/SquareArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class SquareArea
+{
+    static void Main()
+    {
+        int a;
+        int area;
+
+        Console.WriteLine("a = ");
+        a = int.Parse(Console.ReadLine());
+        area = a * a;
+        Console.WriteLine("Square area = {0}", area);
+    }
+}
