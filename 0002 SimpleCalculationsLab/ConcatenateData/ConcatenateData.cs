879b57893c7698210786ce9447d5b42fd55ada2e
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 01:03:16 +0300

    Chapter 2.0

---
+++ 0002 SimpleCalculationsLab/ConcatenateData/ConcatenateData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class ConcatenateData
+{
+    static void Main()
+    {
+        int age;
+        string firstName;
+        string lastName;
+        string town;
+
+        firstName = Console.ReadLine();
+        lastName = Console.ReadLine();
+        age = int.Parse(Console.ReadLine());
+        town = Console.ReadLine();
+
+        Console.WriteLine("You are {0} {1}, a {2}-years old person from {3}.", firstName, lastName, age, town);
+    }
+}
