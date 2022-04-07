a600e288b0044e1f0dc9e14ad411997c209723ab
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 01:05:41 +0300

    Chapter 2.0

---
+++ 0002 SimpleCalculationsLab/ThousandDaysAfterBirth/ThousandDaysAfterBirth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+public class ThousandDaysAfterBirth
+{
+    static void Main()
+    {
+        string date = Console.ReadLine();
+        DateTime dt = DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+        dt = dt.AddDays(1000);
+        Console.WriteLine(dt.ToString("dd-MM-yyyy"));
+    }
+}
