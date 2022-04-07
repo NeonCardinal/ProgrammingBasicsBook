3838189d461bc9f900158517d5a25ddd90b035ce
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 01:04:31 +0300

    Chapter 2.0

---
+++ 0002 SimpleCalculationsLab/InchesToCentimeters/InchesToCentimeters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class InchesToCentimeters
+{
+    static void Main()
+    {
+        double inches;
+        double centimeters;
+
+        Console.WriteLine("Inches = ");
+        inches = double.Parse(Console.ReadLine());
+        centimeters = inches * 2.54;
+        Console.WriteLine("Centimeters = {0}", centimeters);
+    }
+}
