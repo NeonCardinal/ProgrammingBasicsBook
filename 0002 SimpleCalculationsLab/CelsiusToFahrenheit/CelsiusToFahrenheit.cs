9b92ee563d0a84eaad3a3c26a72408c3a4480605
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 01:01:40 +0300

    Chapter 2.0

---
+++ 0002 SimpleCalculationsLab/CelsiusToFahrenheit/CelsiusToFahrenheit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class CelsiusToFahrenheit
+{
+    static void Main()
+    {
+        double celsius;
+        double fahrenheit;
+
+        celsius = double.Parse(Console.ReadLine());
+
+        fahrenheit = celsius * 9 / 5 + 32;
+
+        Console.WriteLine(Math.Round(fahrenheit, 2));
+    }
+}
