add81b9cfbf461b347819db2ce6265e0b061c90b
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-07 17:04:09 +0300

    Chapter 1.0

---
+++ 0001 FirstStepsInProgramming/RectangleArea/RectangleArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class RectangleArea
+{
+    static void Main()
+    {
+        double a;
+        double b;
+
+        a = double.Parse(Console.ReadLine());
+        b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(a * b);
+    }
+}
