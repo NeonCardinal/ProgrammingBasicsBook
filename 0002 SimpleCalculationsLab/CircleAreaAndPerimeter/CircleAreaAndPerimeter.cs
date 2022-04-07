772fd72f15c1369264681358d493d4cc3fe04447
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 01:02:51 +0300

    Chapter 2.0

---
+++ 0002 SimpleCalculationsLab/CircleAreaAndPerimeter/CircleAreaAndPerimeter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class CircleAreaAndPerimeter
+{
+    static void Main()
+    {
+        double r;
+        double area;
+        double perimeter;
+
+        r = double.Parse(Console.ReadLine());
+
+        area = Math.PI * r * r;
+        perimeter = 2 * Math.PI * r;
+
+        Console.WriteLine("Area = " + area);
+        Console.WriteLine("Perimeter = " + perimeter);
+    }
+}
