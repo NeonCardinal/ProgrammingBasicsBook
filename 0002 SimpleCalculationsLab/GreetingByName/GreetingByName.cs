46419285b55d1a7a90f6930a2111abba47a4e5f9
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-08 01:04:07 +0300

    Chapter 2.0

---
+++ 0002 SimpleCalculationsLab/GreetingByName/GreetingByName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class GreetingByName
+{
+    static void Main()
+    {
+        string name;
+
+        name = Console.ReadLine();
+        Console.WriteLine("Hello, {0}!", name);
+    }
+}
