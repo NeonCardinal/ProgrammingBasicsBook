023c4628863d8c2f4df2c30cb8c14cf4640ab5cd
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 19:14:32 +0300

    Chapter 3.0

---
+++ 0004 SimpleConditionsLab/AreaOfFigures/AreaOfFigures.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class AreaOfFigures
+{
+    static void Main()
+    {
+        double a;
+        double b;
+        double area = 0.0;
+        string figure = Console.ReadLine().ToLower();
+
+        if (figure == "square")
+        {
+            a = double.Parse(Console.ReadLine());
+            area = a * a;
+        }
+        else if (figure == "rectangle")
+        {
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            area = a * b;
+        }
+        else if (figure == "circle")
+        {
+            a = double.Parse(Console.ReadLine());
+            area = Math.PI * (a * a);
+        }
+        else if (figure == "triangle")
+        {
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            area = a * b / 2;
+        }
+
+        Console.WriteLine(Math.Round(area, 3));
+    }
+}
