512e3512ce771df82255293ca183f1d0e3544d9c
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 18:57:49 +0300

    Chapter 2.1

---
+++ 0003 SimpleCalculationsExercise/TrainingLab/TrainingLab.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class TrainingLab
+{
+    static void Main()
+    {
+        int rows;
+        int cols;
+        int result;
+        double len;
+        double widht;
+        double lenInCm;
+        double widthInCm;
+
+        len = double.Parse(Console.ReadLine());
+        widht = double.Parse(Console.ReadLine());
+
+        lenInCm = len * 100;
+        widthInCm = widht * 100 - 100;
+
+        rows = (int)(lenInCm / 120);
+        cols = (int)(widthInCm / 70);
+        result = rows * cols - 3;
+
+        Console.WriteLine(result);
+    }
+}
