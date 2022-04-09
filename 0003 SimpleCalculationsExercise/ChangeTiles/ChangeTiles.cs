f4222131256277dcf83936fb20ce71b3e9cf18af
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 18:56:34 +0300

    Chapter 2.1

---
+++ 0003 SimpleCalculationsExercise/ChangeTiles/ChangeTiles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class ChangeTiles
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        double wTile = double.Parse(Console.ReadLine());
+        double lTile = double.Parse(Console.ReadLine());
+        int wBench = int.Parse(Console.ReadLine());
+        int lBench = int.Parse(Console.ReadLine());
+
+        double tileArea = wTile * lTile;
+        double benchArea = wBench * lBench;
+        double result = ((n * n) - benchArea) / tileArea;
+        double time = result * 0.2;
+
+        Console.WriteLine(result);
+        Console.WriteLine(time);
+    }
+}
