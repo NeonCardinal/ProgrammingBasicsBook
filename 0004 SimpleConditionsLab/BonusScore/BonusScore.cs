455dceb113b3e3c67f5f50f09c06931939f5e165
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 19:15:16 +0300

    Chapter 3.0

---
+++ 0004 SimpleConditionsLab/BonusScore/BonusScore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class BonusScore
+{
+    static void Main()
+    {
+        int number;
+        double score;
+
+        score = 0;
+        number = int.Parse(Console.ReadLine());
+
+        if (number <= 100)
+        {
+            score += 5;
+        }
+        else if (number > 100 && number <= 1000)
+        {
+            score = number * 0.20;
+        }
+        else if (number > 1000)
+        {
+            score = number * 0.10;
+        }
+
+        if (number % 2 == 0)
+        {
+            score += 1;
+        }
+
+        if (number % 10 == 5)
+        {
+            score += 2;
+        }
+
+        Console.WriteLine(score);
+        Console.WriteLine(number + score);
+    }
+}
