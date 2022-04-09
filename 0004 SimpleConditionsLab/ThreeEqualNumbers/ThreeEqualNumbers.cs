0992b98d9d9fab3859d7955cf404a610a8f6ba40
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 19:19:10 +0300

    Chapter 3.0

---
+++ 0004 SimpleConditionsLab/ThreeEqualNumbers/ThreeEqualNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class ThreeEqualNumbers
+{
+    static void Main()
+    {
+        int firstNumber;
+        int secondNumber;
+        int thirdNumber;
+
+        firstNumber = int.Parse(Console.ReadLine());
+        secondNumber = int.Parse(Console.ReadLine());
+        thirdNumber = int.Parse(Console.ReadLine());
+
+        if (firstNumber == secondNumber && secondNumber == thirdNumber)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
