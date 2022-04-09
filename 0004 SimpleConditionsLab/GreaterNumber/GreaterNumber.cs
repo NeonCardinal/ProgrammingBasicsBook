9f27c740ac0c05bab017de0c2b2f4ec0fe1369ed
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 19:17:01 +0300

    Chapter 3.0

---
+++ 0004 SimpleConditionsLab/GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class GreaterNumber
+{
+    static void Main()
+    {
+        int firstNumber;
+        int secondNumber;
+
+        firstNumber = int.Parse(Console.ReadLine());
+        secondNumber = int.Parse(Console.ReadLine());
+
+        if (firstNumber > secondNumber)
+        {
+            Console.WriteLine(firstNumber);
+        }
+        else
+        {
+            Console.WriteLine(secondNumber);
+        }
+    }
+}
