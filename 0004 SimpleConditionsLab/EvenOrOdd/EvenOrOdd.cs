4cfc372fcd136ade67bab52be2f5c2a268ab03ed
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 19:15:58 +0300

    Chapter 3.0

---
+++ 0004 SimpleConditionsLab/EvenOrOdd/EvenOrOdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class EvenOrOdd
+{
+    static void Main()
+    {
+        int number;
+
+        number = int.Parse(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("even");
+        }
+        else
+        {
+            Console.WriteLine("odd");
+        }
+    }
+}
