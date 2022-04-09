6757274e906248392d507302272c700acfaa3ff8
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 19:15:36 +0300

    Chapter 3.0

---
+++ 0004 SimpleConditionsLab/EqualWords/EqualWords.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class EqualWords
+{
+    static void Main()
+    {
+        string firstWord;
+        string secondWord;
+
+        firstWord = Console.ReadLine().ToLower();
+        secondWord = Console.ReadLine().ToLower();
+
+        if (firstWord == secondWord)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
