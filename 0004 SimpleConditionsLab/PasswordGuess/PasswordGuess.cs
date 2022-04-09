2551ac7cbb7cedd3073ac7c397d293c9db55fc7e
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 19:18:04 +0300

    Chapter 3.0

---
+++ 0004 SimpleConditionsLab/PasswordGuess/PasswordGuess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class PasswordGuess
+{
+    static void Main()
+    {
+        string password;
+
+        password = Console.ReadLine();
+
+        if (password == "s3cr3t!P@ssw0rd")
+        {
+            Console.WriteLine("Welcome");
+        }
+        else
+        {
+            Console.WriteLine("Wrong password!");
+        }
+    }
+}
