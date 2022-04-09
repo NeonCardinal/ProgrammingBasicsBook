b01695cd95bdd44d8288221fc6ab139b9830401f
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 18:57:30 +0300

    Chapter 2.1

---
+++ 0003 SimpleCalculationsExercise/Money/Money.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Money
+{
+    static void Main()
+    {
+        int bitcoins = int.Parse(Console.ReadLine());
+        double uans = double.Parse(Console.ReadLine());
+        double comission = double.Parse(Console.ReadLine());
+
+        double bitcoinsInLeva = bitcoins * 1168;
+        double uansInleva = uans * 0.15 * 1.76;
+        double totalInEuro = (bitcoinsInLeva + uansInleva) / 1.95;
+        double percentComission = comission / 100;
+        double temp = totalInEuro * percentComission;
+        double result = totalInEuro - temp;
+
+        Console.WriteLine(result);
+    }
+}
