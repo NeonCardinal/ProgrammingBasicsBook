396a066dcf3f684bbd91af839fe026660a16a5b2
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 19:19:33 +0300

    Chapter 3.0

---
+++ 0004 SimpleConditionsLab/TimePlusFifteenMinutes/TimePlusFifteenMinutes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class TimePlusFifteenMinutes
+{
+    static void Main()
+    {
+        int hours;
+        int minutes;
+
+        hours = int.Parse(Console.ReadLine());
+        minutes = int.Parse(Console.ReadLine());
+
+        if (minutes + 15 >= 60)
+        {
+            hours++;
+            minutes = minutes + 15 - 60;
+        }
+        else
+        {
+            minutes += 15;
+        }
+        if (hours >= 24)
+        {
+            hours -= 24;
+        }
+
+        Console.WriteLine("{0:D1}:{1:D2}", hours, minutes);
+    }
+}
