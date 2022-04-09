56e2df649df81e95424bcda650c2eeb17b6519de
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 18:57:08 +0300

    Chapter 2.1

---
+++ 0003 SimpleCalculationsExercise/DailyEarnings/DailyEarnings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class DailyEarnings
+{
+    static void Main()
+    {
+        int workDays = int.Parse(Console.ReadLine());
+        double dailySalary = double.Parse(Console.ReadLine());
+        double correctCurency = double.Parse(Console.ReadLine());
+
+        double monthSalary = workDays * dailySalary;
+        double yearSalary = monthSalary * 12 + monthSalary * 2.5;
+        double vat = yearSalary * 0.25;
+        double neto = (yearSalary - vat) * correctCurency;
+        double result = neto / 365;
+
+        Console.WriteLine(Math.Round(result, 2));
+    }
+}
