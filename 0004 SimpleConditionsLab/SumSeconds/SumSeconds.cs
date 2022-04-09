5063c33bf22a6ef6ad06bdbca86f6fe533d08df9
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 19:18:50 +0300

    Chapter 3.0

---
+++ 0004 SimpleConditionsLab/SumSeconds/SumSeconds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class SumSeconds
+{
+    static void Main()
+    {
+        int firstNumber;
+        int secondNumber;
+        int thirdNumber;
+        int seconds;
+        int minutes;
+
+        minutes = 0;
+        firstNumber = int.Parse(Console.ReadLine());
+        secondNumber = int.Parse(Console.ReadLine());
+        thirdNumber = int.Parse(Console.ReadLine());
+        seconds = firstNumber + secondNumber + thirdNumber;
+
+        if (seconds > 59)
+        {
+            minutes++;
+            seconds -= 60;
+        }
+        if (seconds > 59)
+        {
+            minutes++;
+            seconds -= 60;
+        }
+
+        if (seconds < 10)
+        {
+            Console.WriteLine(minutes + ":" + "0" + seconds);
+        }
+        else
+        {
+            Console.WriteLine(minutes + ":" + seconds);
+        }
+    }
+}
