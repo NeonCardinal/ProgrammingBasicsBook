91052b6bff2775e0409d87302f2db2e8eca7717a
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-09 18:58:11 +0300

    Chapter 2.1

---
+++ 0003 SimpleCalculationsExercise/VegetableMarket/VegetableMarket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class VegetableMarket
+{
+    static void Main()
+    {
+        int vegetableWeight;
+        int fruidWeight;
+        double vegetablePrice;
+        double fruidPrice;
+        double total;
+
+        vegetablePrice = double.Parse(Console.ReadLine());
+        fruidPrice = double.Parse(Console.ReadLine());
+        vegetableWeight = int.Parse(Console.ReadLine());
+        fruidWeight = int.Parse(Console.ReadLine());
+
+        total = (vegetableWeight * vegetablePrice + fruidWeight * fruidPrice) / 1.94;
+
+        Console.WriteLine(total);
+    }
+}
