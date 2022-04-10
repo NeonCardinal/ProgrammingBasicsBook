2f6399a6848cb681aa9691f72855907ad2e0ad25
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-10 22:14:48 +0300

    Chapter 3.1

---
+++ 0005 SimpleConditionsExercise/PipesInPool/PipesInPool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class PipesInPool
+{
+    static void Main()
+    {
+        int volume;
+        int firstPipe;
+        int secondPipe;
+        double hours;
+        double water;
+
+        volume = int.Parse(Console.ReadLine());
+        firstPipe = int.Parse(Console.ReadLine());
+        secondPipe = int.Parse(Console.ReadLine());
+        hours = double.Parse(Console.ReadLine());
+
+        water = (firstPipe + secondPipe) * hours;
+
+        if (water <= volume)
+        {
+            Console.WriteLine($"The pool is {Math.Truncate(water / volume * 100)}% full. " +
+                $"Pipe 1: {Math.Truncate(firstPipe * hours / water * 100)}%. " +
+                $"Pipe 2: {Math.Truncate(secondPipe * hours / water * 100)}%.");
+        }
+        else
+        {
+            Console.WriteLine($"For {hours} hours the pool overflows with {water - volume} liters.");
+        }
+    }
+}
