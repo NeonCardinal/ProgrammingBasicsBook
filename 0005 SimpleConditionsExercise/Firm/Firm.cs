57b5bd46fbb9b11161e9d9c6aa4718dd7df59667
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-10 22:13:56 +0300

    Chapter 3.1

---
+++ 0005 SimpleConditionsExercise/Firm/Firm.cs
@@ -0,0 +1,25 @@
+﻿//Task 005, Chapter 3.1
+using System;
+
+public class Firm
+{
+    static void Main()
+    {
+        int hours = int.Parse(Console.ReadLine());
+        int days = int.Parse(Console.ReadLine());
+        int workers = int.Parse(Console.ReadLine());
+
+        double workDays = days * 0.90;
+        double overtime = workers * 2 * workDays;
+        double workHours = Math.Floor(workDays * 8 + overtime);
+
+        if (hours <= workHours)
+        {
+            Console.WriteLine($"Not enough time!{workHours - hours} hours needed.");
+        }
+        else
+        {
+            Console.WriteLine($"Yes!{hours - workHours} hours left.");
+        }
+    }
+}
