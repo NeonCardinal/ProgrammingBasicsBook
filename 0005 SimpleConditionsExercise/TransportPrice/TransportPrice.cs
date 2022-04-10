f7b75aa904584bfc3c1cc22b2235fd83e1ca079c
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-10 22:15:39 +0300

    Chapter 3.1

---
+++ 0005 SimpleConditionsExercise/TransportPrice/TransportPrice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class TransportPrice
+{
+    static void Main()
+    {
+        int n;
+        string time;
+        double price;
+        double taxiRate;
+
+        price = 0.0;
+        n = int.Parse(Console.ReadLine());
+        time = Console.ReadLine().ToLower();
+
+        if (time == "day")
+        {
+            taxiRate = 0.79;
+        }
+        else
+        {
+            taxiRate = 0.90;
+        }
+
+        if (n < 20)
+        {
+            price = taxiRate * n + 0.70;
+        }
+        else if (n < 100)
+        {
+            price = n * 0.09;
+        }
+        else
+        {
+            price = n * 0.06;
+        }
+
+        Console.WriteLine(price);
+    }
+}
