0ed0c144aefac2da2e30f78ae68c5ff7e9d90241
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-12 01:14:48 +0300

    Chapter 4.0

---
+++ 0006 ComplexConditions/FruitOrVegetable/FruitOrVegetable.cs
@@ -0,0 +1,24 @@
+﻿//Task 004, Chapter 4.0
+using System;
+
+public class FruitOrVegetable
+{
+    static void Main()
+    {
+        string str = Console.ReadLine().ToLower();
+
+        if (str == "banana" || str == "apple" || str == "kiwi" || str == "cherry" ||
+            str == "lemon" || str == "grapes")
+        {
+            Console.WriteLine("fruit");
+        }
+        else if (str == "tomato" || str == "cucumber" || str == "pepper" || str == "carrot")
+        {
+            Console.WriteLine("vegetable");
+        }
+        else
+        {
+            Console.WriteLine("unknown");
+        }
+    }
+}
