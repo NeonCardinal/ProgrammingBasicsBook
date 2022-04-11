f05fd0eaa2e431a93ea797ba76cc7d57941a7196
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-12 01:18:12 +0300

    Chapter 4.0

---
+++ 0006 ComplexConditions/Volleyball/Volleyball.cs
@@ -0,0 +1,23 @@
+﻿//Task 012, Chapter 4.0
+using System;
+
+public class Volleyball
+{
+    static void Main()
+    {
+        string year = Console.ReadLine().ToLower();
+        int holidays = int.Parse(Console.ReadLine());
+        int weekendsHome = int.Parse(Console.ReadLine());
+
+        double sofiaWeekends = (48 - weekendsHome) * (3.0 / 4.0);
+        double playSofia = holidays * (2.0 / 3.0);
+        double playTotal = sofiaWeekends + playSofia + weekendsHome;
+
+        if (year == "leap")
+        {
+            playTotal += playTotal * 0.15;
+        }
+
+        Console.WriteLine(Math.Floor(playTotal));
+    }
+}
