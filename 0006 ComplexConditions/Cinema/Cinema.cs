9ac38b00e769e299768e64d0bc5c94c893fcfa4b
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-12 01:13:49 +0300

    Chapter 4.0

---
+++ 0006 ComplexConditions/Cinema/Cinema.cs
@@ -0,0 +1,35 @@
+﻿//Task 011, Chapter 4.0
+using System;
+
+public class Cinema
+{
+    static void Main()
+    {
+        string type = Console.ReadLine().ToLower();
+        int rows = int.Parse(Console.ReadLine());
+        int cols = int.Parse(Console.ReadLine());
+
+        double income = 0.0;
+
+        switch (type)
+        {
+            case "premiere":
+                {
+                    income = rows * cols * 12.00;
+                }
+                break;
+            case "normal":
+                {
+                    income = rows * cols * 7.50;
+                }
+                break;
+            case "discount":
+                {
+                    income = rows * cols * 5.00;
+                }
+                break;
+        }
+
+        Console.WriteLine($"{income:F2}");
+    }
+}
