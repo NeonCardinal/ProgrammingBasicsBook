9ba7263bc3e4a878d956225cfd64457f69f08b85
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-12 01:12:36 +0300

    Chapter 4.0

---
+++ 0006 ComplexConditions/AnimalType/AnimalType.cs
@@ -0,0 +1,31 @@
+﻿//Task 010, Chapter 4.0
+using System;
+
+public class AnimalType
+{
+    static void Main()
+    {
+        string animal = Console.ReadLine().ToLower();
+
+        switch (animal)
+        {
+            case "dog":
+                {
+                    Console.WriteLine("mammal");
+                }
+                break;
+            case "crocodile":
+            case "tortoise":
+            case "snake":
+                {
+                    Console.WriteLine("reptile");
+                }
+                break;
+            default:
+                {
+                    Console.WriteLine("unknown");
+                }
+                break;
+        }
+    }
+}
