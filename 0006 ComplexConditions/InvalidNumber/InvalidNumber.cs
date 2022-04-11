350a98fbdbf7177a0e0eba090470ed7cf7337c85
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-12 01:15:35 +0300

    Chapter 4.0

---
+++ 0006 ComplexConditions/InvalidNumber/InvalidNumber.cs
@@ -0,0 +1,16 @@
+﻿//Task 005, Chapter 4.0
+using System;
+
+public class InvalidNumber
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        bool inRange = (number >= 100 && number <= 200) || number == 0;
+
+        if (!inRange)
+        {
+            Console.WriteLine("invalid");
+        }
+    }
+}
