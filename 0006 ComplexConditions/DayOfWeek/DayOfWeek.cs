010f8550667cd2492116bba4d406abb5b995eea2
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-12 01:14:23 +0300

    Chapter 4.1

---
+++ 0006 ComplexConditions/DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,54 @@
+﻿//Task 009, Chapter 4.0
+using System;
+
+public class DayOfWeek
+{
+    static void Main()
+    {
+        int day = int.Parse(Console.ReadLine());
+
+        switch (day)
+        {
+            case 1:
+                {
+                    Console.WriteLine("Monday");
+                }
+                break;
+            case 2:
+                {
+                    Console.WriteLine("Tuesday");
+                }
+                break;
+            case 3:
+                {
+                    Console.WriteLine("Wednesday");
+                }
+                break;
+            case 4:
+                {
+                    Console.WriteLine("Thursday");
+                }
+                break;
+            case 5:
+                {
+                    Console.WriteLine("Friday");
+                }
+                break;
+            case 6:
+                {
+                    Console.WriteLine("Saturday");
+                }
+                break;
+            case 7:
+                {
+                    Console.WriteLine("Sunday");
+                }
+                break;
+            default:
+                {
+                    Console.WriteLine("Error!");
+                }
+                break;
+        }
+    }
+}
