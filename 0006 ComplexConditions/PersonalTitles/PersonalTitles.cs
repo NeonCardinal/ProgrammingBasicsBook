b6a0490673028590429706e71b4876d8595e6c78
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-12 01:16:02 +0300

    Chapter 4.0

---
+++ 0006 ComplexConditions/PersonalTitles/PersonalTitles.cs
@@ -0,0 +1,34 @@
+﻿//Task 001, Chapter 4.0
+using System;
+
+public class PersonalTitles
+{
+    static void Main()
+    {
+        double age = double.Parse(Console.ReadLine());
+        string gender = Console.ReadLine().ToLower();
+
+        if (age < 16)
+        {
+            if (gender == "m")
+            {
+                Console.WriteLine("Master");
+            }
+            else if (gender == "f")
+            {
+                Console.WriteLine("Miss");
+            }
+        }
+        else if (age >= 16)
+        {
+            if (gender == "m")
+            {
+                Console.WriteLine("Mr.");
+            }
+            else if (gender == "f")
+            {
+                Console.WriteLine("Ms.");
+            }
+        }
+    }
+}
