2bba07a100349a2185c9ad3fc0d5fc7fccdfdbc9
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-14 14:05:01 +0300

    Chapter 5.1

---
+++ 0009 LoopsExercise/SmartLily/SmartLily.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class SmartLily
+{
+    static void Main()
+    {
+        int lilysAge = int.Parse(Console.ReadLine());
+        double washingMachinePrice = double.Parse(Console.ReadLine());
+        double toyPrice = double.Parse(Console.ReadLine());
+        int toyCounter = 0;
+        int savings = 0;
+        int moneyFromBD = 10;
+
+        for(int i = 1; i <= lilysAge; i++)
+        {
+            if(i % 2 == 0)
+            {
+                savings += (moneyFromBD - 1);
+                moneyFromBD += 10;
+            }
+            else
+            {
+                toyCounter++;
+            }
+        }
+
+        double moneyFromToys = toyCounter * toyPrice;
+        double money = savings + moneyFromToys;
+
+        if(money >= washingMachinePrice)
+        {
+            Console.WriteLine($"Yes! {(money - washingMachinePrice):F2}");
+        }
+        else
+        {
+            Console.WriteLine($"No! {(washingMachinePrice - money):F2}");
+        }
+    }
+}
