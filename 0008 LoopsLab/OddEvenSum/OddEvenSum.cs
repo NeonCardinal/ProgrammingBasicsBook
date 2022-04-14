cef4cc5b0470432bd552930b6293fc652e044a68
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-14 11:38:13 +0300

    Chapter 5.0

---
+++ 0008 LoopsLab/OddEvenSum/OddEvenSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class OddEvenSum
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(i % 2 == 0)
+            {
+                evenSum += number;
+            }
+            else
+            {
+                oddSum += number;
+            }
+        }
+
+        if(evenSum == oddSum)
+        {
+            Console.WriteLine($"Yes, sum = {evenSum}");
+        }
+        else
+        {
+            Console.WriteLine($"No, diff = {Math.Abs(evenSum - oddSum)}");
+        }
+    }
+}
