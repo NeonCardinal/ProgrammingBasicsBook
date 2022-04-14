2f9adabf15787508ca86bc7e3d8661ce0e7600d1
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-14 11:38:37 +0300

    Chapter 5.0

---
+++ 0008 LoopsLab/SumNumbers/SumNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class SumNumbers
+{
+    static void Main()
+    {
+        int sum = 0;
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            sum += number;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
