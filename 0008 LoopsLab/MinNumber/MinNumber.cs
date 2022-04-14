6888471b9fefa050dfc3f54f16761fdb547f0c39
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-14 11:37:18 +0300

    Chapter 5.0

---
+++ 0008 LoopsLab/MinNumber/MinNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class MinNumber
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int min = int.MaxValue;
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number < min)
+            {
+                min = number;
+            }
+        }
+
+        Console.WriteLine(min);
+    }
+}
