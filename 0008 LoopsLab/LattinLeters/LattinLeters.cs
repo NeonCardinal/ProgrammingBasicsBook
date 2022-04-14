0db8977bfb6278c7a0be8e7cbd64c9a8ae9e037a
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-14 11:36:01 +0300

    Chapter 5.0

---
+++ 0008 LoopsLab/LattinLeters/LattinLeters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class LattinLeters
+{
+    static void Main()
+    {
+        for(char symbol = 'a'; symbol <= 'z'; symbol++)
+        {
+            Console.Write($"{symbol} ");
+        }
+
+        Console.WriteLine();
+    }
+}
