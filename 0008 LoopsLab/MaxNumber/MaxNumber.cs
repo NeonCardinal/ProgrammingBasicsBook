8a88d9bd9d66f6eee8c4c26a32df524c191be63a
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-14 11:36:53 +0300

    Chapter 5.0

---
+++ 0008 LoopsLab/MaxNumber/MaxNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class MaxNumber
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int greater = int.MinValue;
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number > greater)
+            {
+                greater = number;
+            }
+        }
+
+        Console.WriteLine(greater);
+    }
+}
