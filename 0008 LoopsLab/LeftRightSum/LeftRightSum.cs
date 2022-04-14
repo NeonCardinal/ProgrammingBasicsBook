d6ec1cb1d1804624b8653e5bd9bb3d0e8429f3ec
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-14 11:36:26 +0300

    Chapter 5.0

---
+++ 0008 LoopsLab/LeftRightSum/LeftRightSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class LeftRightSum
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            leftSum += number;
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            rightSum += int.Parse(Console.ReadLine());
+        }
+
+        if(leftSum == rightSum)
+        {
+            Console.WriteLine($"Yes, sum = {leftSum}");
+        }
+        else
+        {
+            Console.WriteLine($"No, diff = {Math.Abs(leftSum - rightSum)}");
+        }
+    }
+}
