7e11a5ccc1d5a87bb26e7192d0076d6ee2136687
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-17 22:55:05 +0300

    Chapter 6.0

---
+++ 0010 DrawingOnTheConsoleLab/SquareOfStars/SquareOfStars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class SquareOfStars
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < n; i++)
+        {
+            Console.Write("*");
+
+            for(int j = 0; j < n - 1; j++)
+            {
+                Console.Write(" *");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
