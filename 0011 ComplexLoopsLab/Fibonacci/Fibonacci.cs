72d108a00218021d50c85a209468befbda7e19ac
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-18 23:07:12 +0300

    Chapter 7.0

---
+++ 0011 ComplexLoopsLab/Fibonacci/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Fibonacci
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int fZero = 1;
+        int fOne = 1;
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            int fNext = fZero + fOne;
+            fZero = fOne;
+            fOne = fNext;
+        }
+
+        Console.WriteLine(fOne);
+    }
+}
