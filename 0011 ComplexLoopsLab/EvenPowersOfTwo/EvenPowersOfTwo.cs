4e9946e9956efaeee8b36621b4a6c33e2583ca3f
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-18 23:06:33 +0300

    Chapter 7.0

---
+++ 0011 ComplexLoopsLab/EvenPowersOfTwo/EvenPowersOfTwo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class EvenPowersOfTwo
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int number = 1;
+
+        for(int i = 0; i <= n; i += 2)
+        {
+            Console.WriteLine(number);
+            number = number * 2 * 2;
+        }
+    }
+}
