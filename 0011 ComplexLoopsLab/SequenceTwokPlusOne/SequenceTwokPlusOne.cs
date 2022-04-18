fac960d3427509e57d51fa3be89c680ca59b6609
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-18 23:09:41 +0300

    Chapter 7.0

---
+++ 0011 ComplexLoopsLab/SequenceTwokPlusOne/SequenceTwokPlusOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class SequenceTwokPlusOne
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int number = 1;
+
+        while(number <= n)
+        {
+            Console.WriteLine(number);
+
+            number = 2 * number + 1;
+        }
+    }
+}
