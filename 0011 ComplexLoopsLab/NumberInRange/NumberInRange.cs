4fbfef82b34e6871a6ded08ddea43416ea00ca2a
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-18 23:08:10 +0300

    Chapter 7.0

---
+++ 0011 ComplexLoopsLab/NumberInRange/NumberInRange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class NumberInRange
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        while(n < 1 || n > 100)
+        {
+            Console.WriteLine("Invalid number!");
+            n = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(n);
+    }
+}
