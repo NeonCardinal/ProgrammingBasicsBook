ddf4b065be10487715a42f860221bcf28a5a7b9e
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-18 23:05:33 +0300

    Chapter 7.0

---
+++ 0011 ComplexLoopsLab/CheckPrime/CheckPrime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class CheckPrime
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        if(n < 2)
+        {
+            Console.WriteLine("Not prime");
+            return;
+        }
+
+        bool isPrime = true;
+
+        for(int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if(n % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime)
+        {
+            Console.WriteLine("Prime");
+        }
+        else
+        {
+            Console.WriteLine("Not prime");
+        }
+    }
+}
