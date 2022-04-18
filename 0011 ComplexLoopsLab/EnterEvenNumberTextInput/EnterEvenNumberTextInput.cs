9454efe058b4b6856f01fee83c052c4747ff00ed
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-18 23:06:14 +0300

    Chapter 7.0

---
+++ 0011 ComplexLoopsLab/EnterEvenNumberTextInput/EnterEvenNumberTextInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class EnterEvenNumberTextInput
+{
+    static void Main()
+    {
+        while(true)
+        {
+            try
+            {
+                Console.Write("Enter even number: ");
+
+                int n = int.Parse(Console.ReadLine());
+
+                if(n % 2 == 0)
+                {
+                    Console.WriteLine($"Even number entered: {n}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("The number is not even.");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number.");
+            }
+        }
+    }
+}
