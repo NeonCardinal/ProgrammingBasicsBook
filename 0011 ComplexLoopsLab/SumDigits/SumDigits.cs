bfd1dc4639992558f43ad07c61d12a3d6ccd588d
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-18 23:10:04 +0300

    Chapter 7.0

---
+++ 0011 ComplexLoopsLab/SumDigits/SumDigits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class SumDigits
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        do
+        {
+            sum += (n % 10);
+            n /= 10;
+        }
+        while (n > 0);
+
+        Console.WriteLine(sum);
+    }
+}
