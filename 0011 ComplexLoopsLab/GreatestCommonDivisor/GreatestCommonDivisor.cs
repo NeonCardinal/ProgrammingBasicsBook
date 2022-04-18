a5916eeb4515639d977b00789b2bb95c3c87e19a
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-18 23:07:50 +0300

    Chapter 7.0

---
+++ 0011 ComplexLoopsLab/GreatestCommonDivisor/GreatestCommonDivisor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class GreatestCommonDivisor
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        while(b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        Console.WriteLine(a);
+    }
+}
