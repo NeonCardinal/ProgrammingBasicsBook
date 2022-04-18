3e0f662e87126cd4b6bf6e27818f3b91e02d9761
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-18 23:05:53 +0300

    Chapter 7.0

---
+++ 0011 ComplexLoopsLab/EnterEvenNumber/EnterEvenNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class EnterEvenNumber
+{
+    static void Main()
+    {
+        int n = 0;
+
+        while(true)
+        {
+            Console.Write("Enter even number: ");
+
+            n = int.Parse(Console.ReadLine());
+
+            if(n % 2 == 0)
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine($"Even number entered: {n}");
+    }
+}
