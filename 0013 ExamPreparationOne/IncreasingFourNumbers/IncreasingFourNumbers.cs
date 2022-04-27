5457bb40377f9b0d5a881b043a44059eb6b243ec
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-28 02:07:36 +0300

    Chapter 8.0

---
+++ 0013 ExamPreparationOne/IncreasingFourNumbers/IncreasingFourNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class IncreasingFourNumbers
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int count = 0;
+
+        for(int i = a; i <= b; i++)
+        {
+            for(int j = i + 1; j <= b; j++)
+            {
+                for(int k = j + 1; k <= b; k++)
+                {
+                    for(int p = k + 1; p <= b; p++)
+                    {
+                        Console.WriteLine($"{i} {j} {k} {p}");
+                        count++;
+                    }
+                }
+            }
+            //Console.WriteLine();
+        }
+
+        if(count == 0)
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
