dfa22d573de8dde59e0fddbb23625a42ac3a17b8
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-28 01:59:44 +0300

    Chapter 8.0

---
+++ 0013 ExamPreparationOne/RectangleWithStars/RectangleWithStars.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class RectangleWithStars
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(new string('%', n * 2));
+
+        int numRows = n;
+
+        if(n % 2 == 0)
+        {
+            numRows--;
+        }
+
+        for(int i = 0; i < numRows; i++)
+        {
+            Console.Write("%");
+
+            if(i == numRows / 2)
+            {
+                Console.Write(new string(' ', n - 2));
+                Console.Write("**");
+                Console.Write(new string(' ', n - 2));
+            }
+            else
+            {
+                Console.Write(new string(' ', n * 2 - 2));
+            }
+
+            Console.Write("%");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine(new string('%', n * 2));
+    }
+}
