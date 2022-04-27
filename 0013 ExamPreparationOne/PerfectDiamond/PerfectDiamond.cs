b566c282d563fc178e23973626b883e9957bedf3
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-28 01:52:00 +0300

    Chapter 8.0

---
+++ 0013 ExamPreparationOne/PerfectDiamond/PerfectDiamond.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class PerfectDiamond
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= n; i++)
+        {
+            Console.Write(new string(' ', n - i));
+            Console.Write("*");
+            
+            for(int j = 0; j < i - 1; j++)
+            {
+                Console.Write("-*");
+            }
+
+            Console.WriteLine();
+        }
+
+        for(int i = n - 1; i >= 1; i--)
+        {
+            Console.Write(new string(' ', n - i));
+            Console.Write("*");
+
+            for(int j = 1; j < i; j++)
+            {
+                Console.Write("-*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
