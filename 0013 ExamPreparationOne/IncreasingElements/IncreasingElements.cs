48cfe4cdf64b5ea0ede3e601bf5bddd881d04801
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-28 01:51:33 +0300

    Chapter 8.0

---
+++ 0013 ExamPreparationOne/IncreasingElements/IncreasingElements.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class IncreasingElements
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int currentLongest = 0;
+        int longest = 0;
+        int a = 0;
+        int aPrev = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            a = int.Parse(Console.ReadLine());
+
+            if(a > aPrev)
+            {
+                currentLongest++;
+            }
+            else
+            {
+                currentLongest = 1;
+            }
+
+            if(currentLongest > longest)
+            {
+                longest = currentLongest;
+            }
+
+            aPrev = a;
+        }
+
+        Console.WriteLine(longest);
+    }
+}
