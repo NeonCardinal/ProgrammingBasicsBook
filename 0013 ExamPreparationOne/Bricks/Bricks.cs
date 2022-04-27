bf305635835f97fc43da49e73dc92e947d97c3ea
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-28 01:11:27 +0300

    Chapter 8.0

---
+++ 0013 ExamPreparationOne/Bricks/Bricks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Bricks
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int w = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+
+        int bricks = w * m;
+        double courses = Math.Ceiling((double)x / bricks);
+
+        Console.WriteLine(courses);
+    }
+}
