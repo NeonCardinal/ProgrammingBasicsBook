eabd223b1a0c942948f122a9aad05c94a324e842
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-28 01:13:17 +0300

    Chapter 8.0

---
+++ 0013 ExamPreparationOne/SumsThreeNumbers/SumsThreeNumbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class SumsThreeNumbers
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        if(a + b == c)
+        {
+            if(a > b)
+            {
+                Console.WriteLine($"{b} + {a} = {c}");
+            }
+            else
+            {
+                Console.WriteLine($"{a} + {b} = {c}");
+            }          
+        }
+        else if(a + c == b)
+        {
+            if(a > c)
+            {
+                Console.WriteLine($"{c} + {a} = {b}");
+            }
+            else
+            {
+                Console.WriteLine($"{a} + {c} = {b}");
+            }
+        }
+        else if(b + c == a)
+        {
+            if(c > b)
+            {
+                Console.WriteLine($"{c} + {b} = {a}");
+            }
+            else
+            {
+                Console.WriteLine($"{b} + {c} = {a}");
+            }           
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
