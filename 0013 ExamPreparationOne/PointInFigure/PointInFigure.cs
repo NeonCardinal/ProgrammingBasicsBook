5fa389e6f83700f9054b628c10c48c05a4269cfb
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-28 01:12:29 +0300

    Chapter 8.0

---
+++ 0013 ExamPreparationOne/PointInFigure/PointInFigure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class PointInFigure
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        bool firstFugure = x >= 2 && x <= 12 && y >= -3 && y <= 1;
+        bool secondFigure = x >= 4 && x <= 10 && y >= -5 && y <= 3;
+
+        if(firstFugure || secondFigure)
+        {
+            Console.WriteLine("in");
+        }
+        else
+        {
+            Console.WriteLine("out");
+        }
+    }
+}
