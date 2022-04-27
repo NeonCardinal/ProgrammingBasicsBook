94e3eb9d5c6681658e156c6baaae1ff441c002ae
NeonCardinal <nvnnaidenov@gmail.com>
2022-04-28 01:12:54 +0300

    Chapter 8.0

---
+++ 0013 ExamPreparationOne/PointOnSegment/PointOnSegment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class PointOnSegment
+{
+    static void Main()
+    {
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+        int point = int.Parse(Console.ReadLine());
+
+        int left = Math.Min(start, end);
+        int right = Math.Max(start, end);
+        int distanceLeft = Math.Abs(left - point);
+        int distanceRight = Math.Abs(right - point);
+        int minDistance = Math.Min(distanceLeft, distanceRight);
+
+        if(point >= left && point <= right)
+        {
+            Console.WriteLine("in");
+        }
+        else
+        {
+            Console.WriteLine("out");
+        }
+
+        Console.WriteLine(minDistance);
+    }
+}
